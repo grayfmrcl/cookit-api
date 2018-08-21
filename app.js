441992593c48aee3d332abec1ae7bb8e767ce885
Grayfield <grayfieldmm@outlook.com>
2018-08-21 12:01:24 +0700

    init mongoose

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 const routers = require('./routers')
@@ -8,6 +9,14 @@ const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+const db_url = process.env.DB_URL || 'mongodb://127.0.0.1:27017/cook-it'
+mongoose.connect(db_url)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log(`connected to ${db_url}`)
+});
 
 app.use('/', routers)
 
