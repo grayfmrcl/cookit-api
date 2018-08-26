de299eb2b98109eb05f8b0c9d6a196fbf0222797
Grayfield <grayfieldmm@outlook.com>
2018-08-26 22:22:35 +0700

    restructure recipe model

--- models/recipe.js
+++ models/recipe.js
@@ -6,12 +6,17 @@ const recipeSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'User'
   },
-  title: {
+  title: { type: String, default: 'untitled' },
+  img_url: { type: String, default: 'https://images.unsplash.com/photo-1495195134817-aeb325a55b65?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1bdd3a5305d7913b82929130ae81fef6&auto=format&fit=crop&w=1355&q=80' },
+  description: String,
+  ingredients: [{
     type: String,
-  },
-  content: {
-    type: String,
-  },
+    amount: Number,
+    unit: String
+  }],
+  directions: [{ type: String }],
+  estimatedTime: { type: Number, default: 5 },
+  notes: { type: String },
   tags: [{ type: String }],
   updated_at: Date
 })
@@ -25,8 +30,12 @@ recipeSchema.statics.findByUserId = function (user_id) {
   return this.find({ user: user_id })
 }
 
-recipeSchema.statics.findByIdAndUser = function (recipe_id, user_id) {
-  return this.findOne({ _id: recipe_id, user: user_id })
+recipeSchema.statics.findByIdAndUser = function (post_id, user_id) {
+  return this.findOne({ _id: post_id, user: user_id })
+}
+
+recipeSchema.methods.mapSummary = function () {
+  return { _id, user, title, img_url, description, updated_at } = this
 }
 
 module.exports = mongoose.model('Recipe', recipeSchema)
